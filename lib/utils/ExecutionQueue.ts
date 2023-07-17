6c51dcb5ba0ff94bb840d793f60a94a1aafc882d
Thales B. Rodrigues <thalesbrito05@gmail.com>
2023-07-17 13:29:42 -0300

    :wrench: Adjust ExecutionQueue

--- lib/utils/ExecutionQueue.ts
+++ lib/utils/ExecutionQueue.ts
@@ -1,42 +1,52 @@
 /**
- * Class used for delaying execution of a certain method and storing its callings' parameters into a queue
+ * Class used for delaying and ordering execution of a certain function and storing its calls' parameters into a queue
  */
-export class ExecutionQueue<Parameters = any, ReturnType = void> {
-    constructor (public readonly method: (item: Parameters) => ReturnType, public readonly items: Parameters[] = []) {}
+export class ExecutionQueue<T extends (...args: any[]) => any = (...args: any[]) => any> {
+    constructor (public readonly method: T, public readonly items: Parameters<T>[] = []) {}
 
+    /**
+     * Is this queue empty?
+     */
     public get empty() {
         return !this.items.length;
     }
 
     /**
-     * Adds a new item to the end of the queue
+     * The amount of function calls waiting on queue
      */
-    public add(...newItems: Parameters[]) {
-        this.items.push(...newItems);
+    public get length() {
+        return this.items.length;
     }
 
     /**
-     * Removes all items from the queue
+     * Adds a new function call to the end of the queue
+     */
+    public add(...parameters: Parameters<T>) {
+        this.items.push(parameters);
+    }
+
+    /**
+     * Removes all function calls from the queue
      */
     public clear() {
         this.items.splice(0, this.items.length);
     }
 
     /**
-     * Executes this queue's method on the first item and removes it from the queue
+     * Executes the first function call and removes it from the queue
      * @returns The returned value of the method
      */
-    public next(): ReturnType|void {
+    public next() {
         const firstItem = this.items.shift();
-        if (firstItem) return this.method(firstItem);
+        if (firstItem) return this.method(firstItem) as ReturnType<T>|void;
     }
 
     /**
      * Executes this queue's method on all items, from start to finish, clearing the queue
-     * @returns An array containing the returned value mapped for each item on the queue
+     * @returns An array containing the returned values mapped for each function call on the queue
      */
-    public execute(): (ReturnType|void)[] {
-        const returnedValues: (ReturnType|void)[] = [];
+    public run() {
+        const returnedValues: (ReturnType<T>|void)[] = [];
 
         while (!this.empty) {
             returnedValues.unshift(this.next());
